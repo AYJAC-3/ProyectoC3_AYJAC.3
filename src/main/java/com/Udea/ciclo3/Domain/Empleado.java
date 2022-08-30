4fd0b9fec1002d2982986ad222a05fb61a9a30b7
Cristian David Gomez Arcila <96919272+CristianDGomez@users.noreply.github.com>
2022-08-30 00:25:36 -0300

    Revert "Development cristian"

--- src/main/java/com/Udea/ciclo3/Domain/Empleado.java
+++ src/main/java/com/Udea/ciclo3/Domain/Empleado.java
@@ -1,48 +1,4 @@
 package com.Udea.ciclo3.Domain;
 
-import lombok.Getter;
-import lombok.Setter;
-
 public class Empleado {
-
-
-    @Getter
-    @Setter
-    private String nombreEmpleado;
-    @Getter @Setter
-    private String correoEmpleado;
-
-
-    private Empresa empresaPertenece;
-    @Getter @Setter
-    private String rolEmpleado;
-
-    public Empleado() {
-    }
-
-    public Empleado(String nombreEmpleado, String correoEmpleado, Empresa empresaPertenece, String rolEmpleado) {
-        this.nombreEmpleado = nombreEmpleado;
-        this.correoEmpleado = correoEmpleado;
-        this.empresaPertenece = empresaPertenece;
-        this.rolEmpleado = rolEmpleado;
-    }
-
-    public void setempresaPertenece(Empresa empresa) {
-        this.empresaPertenece = empresa;
-    }
-
-    public Empresa getEmpresaPertenece() {
-        return empresaPertenece;
-    }
-
-    @Override
-    public String toString() {
-        return
-                "\tNombre del empleado : " + nombreEmpleado + "\n" +
-                        "\tCorreo del empleado : " + correoEmpleado + "\n" +
-                        "\tRol del empleado : " + rolEmpleado + "\n"+
-                        "Empresa a la que Pertenece:" + empresaPertenece + "\n";
-
-    }
-    //intento1000
 }
