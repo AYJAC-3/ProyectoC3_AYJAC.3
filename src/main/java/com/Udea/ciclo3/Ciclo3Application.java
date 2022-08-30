74cd90e9775f89afd2f6a425450230434cabcdff
CristianDGomez <Cristian.dgarcila@gmail.com>
2022-08-30 19:44:31 -0300

    Rama Development-Cristian v.4 creacion de repositorios y servicios de la empresa

--- src/main/java/com/Udea/ciclo3/Ciclo3Application.java
+++ src/main/java/com/Udea/ciclo3/Ciclo3Application.java
@@ -7,9 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication(exclude ={SecurityAutoConfiguration.class})
 public class Ciclo3Application {
 
---
+++ src/main/java/com/Udea/ciclo3/controller/ControllerFull.java
@@ -0,0 +1,24 @@
+package com.Udea.ciclo3.controller;
+
+import com.Udea.ciclo3.Domain.Empresa;
+import com.Udea.ciclo3.service.EmpresaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+import java.util.List;
+
+@Controller
+public class ControllerFull {
+    @Autowired
+    EmpresaService empresaService;
+
+    @GetMapping ({"/Empresas","/verEmpresa"})
+    public String viewEmpresas(Model model){
+        List<Empresa> ListaEmpresa = empresaService.getAllEmpresas();
+        model.addAttribute(  "emplist",ListaEmpresa);
+        return  "verEmpresas";
+    }
+}
